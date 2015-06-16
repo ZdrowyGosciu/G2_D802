795084c62618ee8b0798a662919e263d4929b694
Nick <nb00870@gmail.com>
2015-06-16 10:12:00 -0700

    Euphoria

--- cm.mk
+++ cm.mk
@@ -1,25 +1,49 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+# Boot animatio# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit device configuration
+# Inherit from d802 device
 $(call inherit-product, device/lge/d802/d802.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+PRODUCT_NAME := eos_d802
 PRODUCT_DEVICE := d802
-PRODUCT_NAME := cm_d802
-PRODUCT_BRAND := LGE
-PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := LG-d802
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=lge/g2_open_com/g2:4.4.2/KOT49I.D80220h/D80220h.1413914494:user/release-keys \
-    PRIVATE_BUILD_DESC="g2_open_com-user 4.4.2 KOT49I.D80220h D80220h.1413914494 release-keys"
+#PRODUCT_GMS_CLIENTID_BASE :=
 
-PRODUCT_PACKAGES += Torch
+PRODUCT_BRAND := lge
+TARGET_VENDOR := lge
+TARGET_VENDOR_PRODUCT_NAME := d802
+TARGET_VENDOR_DEVICE_NAME := LG-d802
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=LG-d802 PRODUCT_NAME=d802
+
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+## Use the latest approved GMS identifiers unless running a signed build
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.4.2/KOT49I.D80220h/D80220h.1413914494:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.4.2 KOT49I.D80220h D80220h.1413914494 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+
+
+
+
